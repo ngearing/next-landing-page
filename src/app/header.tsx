3a957c9c9f85b2d51033c48ec22e190e258ca2f9
Nathan Gearing <natgearing@gmail.com>
2024-02-26 09:35:32 +1100

    header - structure

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,7 +1,18 @@
 export default function Header() {
 	return (
-		<header className="bg-white color-black flex-col items-center justify-between p-24">
-			Logo Socials
+		<header className="bg-white text-black flex items-center justify-between p-24">
+			<div className="site-branding">Logo</div>
+			<div className="site-socials flex gap-2">
+				<a className="social-link" href="#">
+					Facebook
+				</a>
+				<a className="social-link" href="#">
+					Twitter
+				</a>
+				<a className="social-link" href="#">
+					Instagram
+				</a>
+			</div>
 		</header>
 	);
 }
